9ab34e047a1d37f066a40099935f83e2959e98cf
nagapremika <sadhupremika@gmail.com>
2019-07-24 15:04:03 +0530

    Commiting task5

--- src/main/java/com/stackroute/controller/MuzixController.java
+++ src/main/java/com/stackroute/controller/MuzixController.java
@@ -1,6 +1,8 @@
 package com.stackroute.controller;
 
 import com.stackroute.domain.Muzix;
+import com.stackroute.exception.TrackAlreadyExistsException;
+import com.stackroute.exception.TrackNotFoundException;
 import com.stackroute.service.MuzixService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +26,7 @@ public class MuzixController {
             muzixService.saveMuzix(muzix);
             responseEntity=new ResponseEntity<String>("successfully created", HttpStatus.CREATED);
         }
-        catch (Exception exception)
+        catch (TrackAlreadyExistsException exception)
         {
             responseEntity=new ResponseEntity<String>(exception.getMessage(), HttpStatus.CONFLICT);
         }
@@ -39,18 +41,39 @@ public class MuzixController {
     }
 //Handles delete operation
     @PostMapping("delete")
-    public ResponseEntity<?> deleteMuzix(@RequestBody Muzix muzix)
-    {
-        return new ResponseEntity<Boolean>(muzixService.deleteMuzix(muzix.getId()),HttpStatus.OK);
+    public ResponseEntity<?> deleteMuzix(@RequestBody Muzix muzix) {
+        ResponseEntity responseEntity;
+        try {
+            return new ResponseEntity<Boolean>(muzixService.deleteMuzix(muzix.getId()), HttpStatus.OK);
+        } catch (TrackNotFoundException exception) {
+            responseEntity = new ResponseEntity<String>(exception.getMessage(), HttpStatus.CONFLICT);
+        }
+        return responseEntity;
     }
 //Handles update operation
     @PostMapping("update")
-    public ResponseEntity<?> updateMuzix(@RequestBody Muzix muzix)
-    {
-        return new ResponseEntity<Boolean>(muzixService.updateMuzix(muzix),HttpStatus.OK);
+    public ResponseEntity<?> updateMuzix(@RequestBody Muzix muzix) {
+        ResponseEntity responseEntity;
+        try {
+
+            return new ResponseEntity<Boolean>(muzixService.updateMuzix(muzix), HttpStatus.OK);
+        } catch (TrackNotFoundException exception) {
+            responseEntity = new ResponseEntity<String>(exception.getMessage(), HttpStatus.CONFLICT);
+        }
+        return responseEntity;
     }
 
 
 
-
+//Handles TrackByName
+@PostMapping("trackByName")
+    public ResponseEntity<?> trackByName(@RequestBody Muzix muzix)
+{
+    return new ResponseEntity<>(muzixService.trackByName(muzix.getName()),HttpStatus.OK);
 }
+
+
+
+
+
+    }
--- src/main/java/com/stackroute/service/MuzixServieImpl.java
+++ src/main/java/com/stackroute/service/MuzixServieImpl.java
@@ -1,6 +1,8 @@
 package com.stackroute.service;
 
 import com.stackroute.domain.Muzix;
+import com.stackroute.exception.TrackAlreadyExistsException;
+import com.stackroute.exception.TrackNotFoundException;
 import com.stackroute.repository.MuzixRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,9 +19,13 @@ public class MuzixServieImpl implements MuzixService {      //Implements MuzixSe
     }
 
     @Override       //overrides saveMuzix
-    public Muzix saveMuzix(Muzix muzix) {
-    return muzixRepository.save(muzix);
-
+    public Muzix saveMuzix(Muzix muzix) throws TrackAlreadyExistsException {
+        if(muzixRepository.existsById(muzix.getId())) {
+            return muzixRepository.save(muzix);
+        }
+        else{
+            throw new TrackAlreadyExistsException("track already exists");
+        }
     }
 
     @Override       //overrides getAllMuzix
@@ -29,19 +35,37 @@ public class MuzixServieImpl implements MuzixService {      //Implements MuzixSe
     }
 
     @Override       //overrides updateMuzix
-    public boolean updateMuzix(Muzix muzix) {
-        muzix.setId(muzix.getId());
-        muzix.setName(muzix.getName());
-        muzix.setTrack(muzix.getTrack());
-        muzixRepository.save(muzix);
-        return true;
+    public boolean updateMuzix(Muzix muzix) throws TrackNotFoundException {
+        if(muzixRepository.existsById(muzix.getId())) {
+            muzix.setId(muzix.getId());
+            muzix.setName(muzix.getName());
+            muzix.setTrack(muzix.getTrack());
+            muzixRepository.save(muzix);
+            return true;
+        }
+        else{
+            throw new TrackNotFoundException("Track not found");
+        }
 
     }
 
-    @Override       //overrides deleteMuzix
-    public boolean deleteMuzix(int id) {
-        muzixRepository.deleteById(id);
-        return true;
 
+    @Override       //overrides deleteMuzix
+    public boolean deleteMuzix(int id) throws TrackNotFoundException {
+        if(muzixRepository.existsById(id)) {
+            muzixRepository.deleteById(id);
+            return true;
+        }
+        else{
+            throw new TrackNotFoundException("Track not found");
+        }
+
+    }
+
+    @Override       //overrides trackByName
+    public List<Muzix> trackByName(String name) {
+        Muzix muzix=new Muzix();
+        muzix.setName(name);
+        return muzixRepository.trackByName(muzix.getName());
     }
 }
