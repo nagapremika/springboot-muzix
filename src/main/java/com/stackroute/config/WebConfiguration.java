19f6bff119f271b335b0fbc2c3597a5ec8498c92
nagapremika <51943079+nagapremika@users.noreply.github.com>
2019-07-24 11:50:39 +0530

    Update WebConfiguration.java

--- src/main/java/com/stackroute/config/WebConfiguration.java
+++ src/main/java/com/stackroute/config/WebConfiguration.java
@@ -5,13 +5,13 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration      //Configuration class
 public class WebConfiguration {
     @Bean
-    ServletRegistrationBean h2servletRegistration()
+    ServletRegistrationBean h2servletRegistration()     //Bean creation for ServletRegistrationBean
     {
         ServletRegistrationBean registrationBean=new ServletRegistrationBean(new WebServlet());
-        registrationBean.addUrlMappings("/console/*");
+        registrationBean.addUrlMappings("/console/*");  //mapping
         return registrationBean;
     }
 }
