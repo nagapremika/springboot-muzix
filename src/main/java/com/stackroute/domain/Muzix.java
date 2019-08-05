1d72fd46cbc85d4b2781ed8a3eaf1840531c4b59
nagapremika <51943079+nagapremika@users.noreply.github.com>
2019-08-05 17:25:31 +0530

    Update Muzix.java

--- src/main/java/com/stackroute/domain/Muzix.java
+++ src/main/java/com/stackroute/domain/Muzix.java
@@ -1,18 +1,12 @@
 package com.stackroute.domain;
-
-<<<<<<< HEAD
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-=======
->>>>>>> 97c2db4ee8a10d60b202a446d2c697a10b2049e1
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-<<<<<<< HEAD
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -25,47 +19,7 @@ public class Muzix {
     private String trackName;
 
     private String comments;
-=======
-public class Muzix {    //Creates an entity Muzix
-    @Id
-    private int id;
-    private String name;
-    private String track;
-//Parameterized constructor
-    public Muzix(int id, String name, String track) {
-        this.id = id;
-        this.name = name;
-        this.track = track;
-    }
-//Default constructor
-    public Muzix() {
-    }
-//setters and getters
-    public int getId() {
-        return id;
-    }
 
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTrack() {
-        return track;
-    }
-
-    public void setTrack(String track) {
-        this.track = track;
-    }
-
-
->>>>>>> 97c2db4ee8a10d60b202a446d2c697a10b2049e1
 }
+
+   
