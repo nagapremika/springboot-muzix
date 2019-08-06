e9f31de21d7cf9255037258e6ab00abdd7df8714
nagapremika <51943079+nagapremika@users.noreply.github.com>
2019-08-06 21:58:12 +0530

    Update MuzixService.java

--- src/main/java/com/stackroute/service/MuzixService.java
+++ src/main/java/com/stackroute/service/MuzixService.java
@@ -1,14 +1,12 @@
 package com.stackroute.service;
-
 import com.stackroute.domain.Muzix;
 import com.stackroute.exception.TrackNotFoundException;
-
 import java.util.List;
 
 public interface MuzixService { //interface with method declarations
     Muzix saveTrack(Muzix muzix);
     List<Muzix> getAllTracks();
-    List<Muzix> deleteTrack(int trackId);
+    Muzix deleteTrack(int trackId);
     Muzix updateTrack(Muzix muzix,int id) throws TrackNotFoundException;
      void getList();
 
