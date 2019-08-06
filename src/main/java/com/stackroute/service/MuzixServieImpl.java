1d36a07722b16e1c7e129f3e79a7b8c9df778df7
nagapremika <51943079+nagapremika@users.noreply.github.com>
2019-08-06 21:43:01 +0530

    Update MuzixServieImpl.java

--- src/main/java/com/stackroute/service/MuzixServieImpl.java
+++ src/main/java/com/stackroute/service/MuzixServieImpl.java
@@ -1,17 +1,14 @@
 package com.stackroute.service;
-
 import com.stackroute.domain.Muzix;
 import com.stackroute.repository.MuzixRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
 public class MuzixServieImpl implements MuzixService {      //Implements MuzixService
     @Autowired
     MuzixRepository muzixRepository;
-
     public MuzixServieImpl(MuzixRepository muzixRepository) {
         this.muzixRepository = muzixRepository;
     }
@@ -19,7 +16,6 @@ public class MuzixServieImpl implements MuzixService {      //Implements MuzixSe
     @Override       //overrides saveMuzix
     public Muzix saveMuzix(Muzix muzix) {
     return muzixRepository.save(muzix);
-
     }
 
     @Override       //overrides getAllMuzix
@@ -33,15 +29,11 @@ public class MuzixServieImpl implements MuzixService {      //Implements MuzixSe
         muzix.setId(muzix.getId());
         muzix.setName(muzix.getName());
         muzix.setTrack(muzix.getTrack());
-        return muzixRepository.save(muzix);
-        
-
-    }
+        return muzixRepository.save(muzix);     
+   }
 
     @Override       //overrides deleteMuzix
     public Muzix deleteMuzix(int id) {
-        return muzixRepository.deleteById(id);
-        
-
+        return muzixRepository.deleteById(id);     
     }
 }
