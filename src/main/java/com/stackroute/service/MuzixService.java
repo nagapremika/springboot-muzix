c3748857549b092967a6e43319cfd2534cd9b80d
nagapremika <51943079+nagapremika@users.noreply.github.com>
2019-08-06 21:57:33 +0530

    Update MuzixService.java

--- src/main/java/com/stackroute/service/MuzixService.java
+++ src/main/java/com/stackroute/service/MuzixService.java
@@ -1,12 +1,10 @@
 package com.stackroute.service;
-
 import com.stackroute.domain.Muzix;
-
 import java.util.List;
 
 public interface MuzixService {     //Interface MuzixService
     public Muzix saveMuzix(Muzix muzix);
     public List<Muzix> getAllMuzix();
-    public boolean updateMuzix(Muzix muzix);
-    public boolean deleteMuzix(int muzoxId);
+    public Muzix updateMuzix(Muzix muzix);
+    public Muzix deleteMuzix(int muzoxId);
 }
